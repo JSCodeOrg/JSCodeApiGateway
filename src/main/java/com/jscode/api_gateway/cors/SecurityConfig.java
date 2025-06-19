d4c742833f986c3d419c67822775ce1e381c09ca
StivenRuiz2005 <carlosruizrojas2005@gmail.com>
2025-06-18 23:25:55 -0500

    Dockerizacion

--- src/main/java/com/jscode/api_gateway/cors/SecurityConfig.java
+++ src/main/java/com/jscode/api_gateway/cors/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200")); 
+        config.setAllowedOrigins(List.of("https://6d0f-152-203-14-224.ngrok-free.app")); 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
